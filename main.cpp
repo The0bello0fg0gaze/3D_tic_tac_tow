14a1d66a382de4c53a07d758107259932eb8a643
Rishav_Giri <rishavgiri509@gmail.com>
2025-03-31 20:52:28 +0530

    made the camera object as the pass in function and need to auto update the camera.up variable

--- main.cpp
+++ main.cpp
@@ -5,38 +5,42 @@
 #include "rcamera.h"
 using namespace std;
 vector<int> relative={3,2,1};
-void UpdateRelative(int move);
-void SetPos(Vector3 &pos);
+void UpdateRelative(int move,int mode, Camera &camera);
+void SetPos(Camera &camera);
 
-void SetPos(Vector3 &pos){
+void SetPos(Camera &camera){
     int set = relative.at(0);
     if(set == 1){
-        pos = (Vector3){6.1f,1.1f,1.1f};
+        camera.position = (Vector3){6.1f,1.0f,1.0f};
     }else if(set == 2){
-        pos = (Vector3){1.1f,6.1f,1.1f};
+        camera.position = (Vector3){1.1f,6.0f,1.0f};
     }else if(set == 3){
-        pos = (Vector3){1.1f,1.1f,6.1f};
+        camera.position = (Vector3){1.1f,1.0f,6.0f};
     }
     else if(set == -1){
-        pos = (Vector3){-6.1f,1.1f,1.1f};
+        camera.position = (Vector3){-4.1f,1.0f,1.0f};
     }else if(set == -2){
-        pos = (Vector3){1.1f,-6.1f,1.1f};
+        camera.position = (Vector3){1.1f,-4.0f,1.0f};
     }else if(set == -3){
-        pos = (Vector3){1.1f,1.1f,-6.1f};
+        camera.position = (Vector3){1.1f,1.0f,-4.0f};
     }
 }
-void UpdateRelative(int move){
-    if((move >= 2)&&(move <= 1)){
+void UpdateRelative(int move,int mode, Camera &camera){
+    if((move <= 2)&&(move >= 1)&&(mode == 0)){
         int temp = relative.at(0);
         relative.at(0) = relative.at(move);
         relative.at(move) = 0-temp;
+    }
+    else if((move <= 2)&&(move >= 1)&&(mode == 1)){
+        int temp = relative.at(move);
+        relative.at(move) = relative.at(0);
+        relative.at(0) = 0-temp;
     }
 }
 
 int main(void)
 {
     vector<vector<vector<int>>> map(3,vector<vector<int>>(3,vector<int>(3,0)));
-    Vector3 pos = { 1.1f, 1.1f, 6.1f };
     int move = 1; 
     int screenWidth = 800;
     const int screenHeight = 450;
@@ -45,7 +49,7 @@ int main(void)
 
     // Define the camera to look into our 3d world (position, target, up vector)
     Camera camera = { 0 };
-    camera.position = pos;  // Camera position
+    camera.position = (Vector3){ 1.1f, 1.0f, 6.0f };  // Camera position
     camera.target = (Vector3){ 1.0f, 1.0f, 1.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
     camera.fovy = 60.0f;                                // Camera field-of-view Y
@@ -56,16 +60,30 @@ int main(void)
     SetTargetFPS(30);                   // Set our game to run at 60 frames-per-second
     while(!WindowShouldClose()){
         UpdateCamera(&camera,0);
-        if(IsKeyUp(KEY_UP)){
-            UpdateRelative(1);
-            SetPos(pos);
-            std::cout<<pos.x<<pos.y<<pos.z<<std::endl;
-        }else if(IsKeyUp(KEY_DOWN)){
-            UpdateRelative(2);
-            SetPos(pos);
-            std::cout<<pos.x<<pos.y<<pos.z<<std::endl;
+        if(IsKeyPressed(KEY_UP)){
+            UpdateRelative(1,0,camera);
+            SetPos(camera);
+            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
+        }else if(IsKeyPressed(KEY_DOWN)){
+            UpdateRelative(1,1,camera);
+            SetPos(camera);
+            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
+        }else if(IsKeyPressed(KEY_RIGHT)){
+            UpdateRelative(2,0,camera);
+            SetPos(camera);
+            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
+        }else if(IsKeyPressed(KEY_LEFT)){
+            UpdateRelative(2,1,camera);
+            SetPos(camera);
+            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
+        }else if(IsKeyPressed(KEY_A)){
+            camera.up = (Vector3){1,0,0};
+        }else if(IsKeyPressed(KEY_S)){
+            camera.up = (Vector3){0,1,0};
+        }else if(IsKeyPressed(KEY_D)){
+            camera.up = (Vector3){0,0,1};
         }
-        camera.position = pos;
+        map[1][0][0]=-1;
         BeginDrawing();
         ClearBackground(RAYWHITE);
                 BeginMode3D(camera); // Start 3D mode
