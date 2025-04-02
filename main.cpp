0d889e72f60c7df908f60dfbfb00d182330b764c
Rishav_Giri <rishavgiri509@gmail.com>
2025-04-02 23:51:35 +0530

    move block still pending think more about adding making it move respect to the camera

--- main.cpp
+++ main.cpp
@@ -4,28 +4,71 @@
 #include "raylib.h"
 #include "rcamera.h"
 using namespace std;
+Vector3 rectpos = {1.0f, 1.0f, 1.0f};
 vector<int> relative={3,2,1};
+void DisplayMap(vector<vector<vector<int>>> &map);
 void UpdateRelative(int move,int mode, Camera &camera);
 void SetPos(Camera &camera);
+void MoveBlock(int move,int cpos,int mode);
+void SetBock(vector<vector<vector<int>>> &map);
+void MoveBlock(int move, Camera camera){
+    float curr = (relative.at(0)*relative.at(0))/relative.at(0);                   
+    float up = ((camera.up.x*1) +(camera.up.y*2)+(camera.up.z*3));                 
+    float right = 6 - up - curr;                                                  
+    vector<float> tempview = {0,0,0}; 
+    if((move == 1)){ // left 
+        
+    }else if((move == -1)){ // right 
+
+    }else if((move == 2)){ // up
+        
+    }else if((move == -2)){ // down
+
+    }
+}
+
+
+void DisplayMap(vector<vector<vector<int>>> &map){
+    for(float x=0; x<3; x++){
+        for(float y=0; y<3; y++){
+            for(float z=0; z<3; z++){
+                DrawCubeWires(Vector3 {x,y,z}, 1.0f, 1.0f, 1.0f, BLACK);
+                if(map[x][y][z] == 1){
+                    DrawCube(Vector3 {x,y,z}, 0.5f, 0.5f, 0.5f, RED);
+                }
+                else if(map[x][y][z] == -1){
+                    DrawCube(Vector3 {x,y,z}, 0.5f, 0.5f, 0.5f, BLUE);
+                }
+            }
+        }
+    }   
+}
 
 void SetPos(Camera &camera){
     int set = relative.at(0);
     if(set == 1){
-        camera.position = (Vector3){6.1f,1.0f,1.0f};
+        camera.position = (Vector3){6.0f,1.0f,1.0f};
     }else if(set == 2){
-        camera.position = (Vector3){1.1f,6.0f,1.0f};
+        camera.position = (Vector3){1.0f,6.0f,1.0f};
     }else if(set == 3){
-        camera.position = (Vector3){1.1f,1.0f,6.0f};
+        camera.position = (Vector3){1.0f,1.0f,6.0f};
     }
     else if(set == -1){
-        camera.position = (Vector3){-4.1f,1.0f,1.0f};
+        camera.position = (Vector3){-4.0f,1.0f,1.0f};
     }else if(set == -2){
-        camera.position = (Vector3){1.1f,-4.0f,1.0f};
+        camera.position = (Vector3){1.0f,-4.0f,1.0f};
     }else if(set == -3){
-        camera.position = (Vector3){1.1f,1.0f,-4.0f};
+        camera.position = (Vector3){1.0f,1.0f,-4.0f};
     }
 }
 void UpdateRelative(int move,int mode, Camera &camera){
+    if((move == 1)){                                        //camera.up movement manager 
+        vector<float> up = {0.0f, 0.0f, 0.0f};
+        float temp1 = relative.at(0);
+        if(0<temp1){up.at((temp1)-1) = -1;}
+        else{up.at((0-temp1)-1) = 1;}
+        camera.up = (Vector3) {up[0],up[1],up[2]};
+    }
     if((move <= 2)&&(move >= 1)&&(mode == 0)){
         int temp = relative.at(0);
         relative.at(0) = relative.at(move);
@@ -38,14 +81,17 @@ void UpdateRelative(int move,int mode, Camera &camera){
     }
 }
 
+
+
 int main(void)
 {
-    vector<vector<vector<int>>> map(3,vector<vector<int>>(3,vector<int>(3,0)));
+    vector<vector<vector<int>>> map(3,vector<vector<int>>(3,vector<int>(3,0))); // the map of the cube we will be using
+    int player = 1;
     int move = 1; 
     int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
+    InitWindow(screenWidth, screenHeight, "Raylib 3D Tic Tac Tow Game (PVE)");
 
     // Define the camera to look into our 3d world (position, target, up vector)
     Camera camera = { 0 };
@@ -60,46 +106,39 @@ int main(void)
     SetTargetFPS(30);                   // Set our game to run at 60 frames-per-second
     while(!WindowShouldClose()){
         UpdateCamera(&camera,0);
-        if(IsKeyPressed(KEY_UP)){
+        if(IsKeyPressed(KEY_UP)){          //Movement of the camera (Map)
             UpdateRelative(1,0,camera);
             SetPos(camera);
-            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
         }else if(IsKeyPressed(KEY_DOWN)){
             UpdateRelative(1,1,camera);
             SetPos(camera);
-            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
         }else if(IsKeyPressed(KEY_RIGHT)){
             UpdateRelative(2,0,camera);
             SetPos(camera);
-            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
         }else if(IsKeyPressed(KEY_LEFT)){
             UpdateRelative(2,1,camera);
             SetPos(camera);
-            std::cout<<camera.position.x<<"/"<<camera.position.y<<"/"<<camera.position.z<<std::endl;
-        }else if(IsKeyPressed(KEY_A)){
-            camera.up = (Vector3){1,0,0};
-        }else if(IsKeyPressed(KEY_S)){
-            camera.up = (Vector3){0,1,0};
+        }
+        if(IsKeyPressed(KEY_A)){             // Movement for player Character
         }else if(IsKeyPressed(KEY_D)){
+            camera.up = (Vector3){0,1,0};
+        }else if(IsKeyPressed(KEY_W)){
+            camera.up = (Vector3){0,0,1};
+        }else if(IsKeyPressed(KEY_S)){
+            camera.up = (Vector3){0,0,1};
+        }else if(IsKeyPressed(KEY_Q)){
+            camera.up = (Vector3){0,0,1};
+        }else if(IsKeyPressed(KEY_E)){
+            camera.up = (Vector3){0,0,1};
+        }else if(IsKeyPressed(KEY_SPACE)){
             camera.up = (Vector3){0,0,1};
         }
-        map[1][0][0]=-1;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
                 BeginMode3D(camera); // Start 3D mode
-                    for(float x=0; x<3; x++){
-                        for(float y=0; y<3; y++){
-                            for(float z=0; z<3; z++){
-                                DrawCubeWires(Vector3 {x,y,z}, 1.0f, 1.0f, 1.0f, BLACK);
-                                if(map[x][y][z] == 1){
-                                    DrawCube(Vector3 {x,y,z}, 0.5f, 0.5f, 0.5f, RED);
-                                }
-                                else if(map[x][y][z] == -1){
-                                    DrawCube(Vector3 {x,y,z}, 0.5f, 0.5f, 0.5f, BLUE);
-                                }
-                            }
-                        }
-                    }   
+                    DrawCube(rectpos, 0.5f, 0.5f, 0.5f, BLACK);
+                    DisplayMap(map);
                 EndMode3D(); // End 3D mode
                 DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
                 DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 30, 10, BLACK);
